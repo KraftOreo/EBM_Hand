907c189fc059fea261f0ac123d3219ac7c586106
kraftoreo <976539567@qq.com>
2020-12-10 22:48:33 +0800

    ebm model

--- commit.sh
+++ commit.sh
@@ -2,6 +2,9 @@ rm -r .vscode
 rm -r .idea
 rm -rf __pycache__
 rm -rf *_imgs
+rm -r ./grasp_generation/batch/*
+rm -r ./grasp_generation/output/*
+
 git add .
 git commit -m "$1"
 git push origin main
